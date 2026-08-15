5fdf15d1ca2566463cc0868ac6f8587de83cdc17
denevieirazz <douglasvieira2544@gmail.com>
2026-08-15 17:41:00 -0300

    fix(browser): corrigir aliases e IO do probe fisico

--- desktop/CloudOS.Browser.PhysicalProbe/GlobalUsings.cs
+++ desktop/CloudOS.Browser.PhysicalProbe/GlobalUsings.cs
@@ -1,1 +1,4 @@
+global using System.IO;
+global using Application = System.Windows.Application;
+global using Clipboard = System.Windows.Clipboard;
 global using TextBox = System.Windows.Controls.TextBox;
