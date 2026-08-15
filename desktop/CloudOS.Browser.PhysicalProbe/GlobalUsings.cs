710a5da64e0645d2b874e2cc9eea35561b280798
denevieirazz <douglasvieira2544@gmail.com>
2026-08-15 20:31:07 -0300

    fix(browser): restaurar aliases WPF e IO do probe

--- desktop/CloudOS.Browser.PhysicalProbe/GlobalUsings.cs
+++ desktop/CloudOS.Browser.PhysicalProbe/GlobalUsings.cs
@@ -1,2 +1,5 @@
+global using System.IO;
 global using CloudOS.Host.Browser;
 global using TextBox = System.Windows.Controls.TextBox;
+global using Application = System.Windows.Application;
+global using Clipboard = System.Windows.Clipboard;
