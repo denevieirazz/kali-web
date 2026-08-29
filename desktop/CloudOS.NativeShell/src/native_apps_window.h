d9e17426d49020d82443e06f7eceedd8f1780b08
denevieirazz <douglasvieira2544@gmail.com>
2026-08-29 10:26:00 -0300

    fix(native): expose app catalog value type to native helpers

--- desktop/CloudOS.NativeShell/src/native_apps_window.h
+++ desktop/CloudOS.NativeShell/src/native_apps_window.h
@@ -1,19 +1,54 @@
 #pragma once
+
 #include <Windows.h>
+
 #include <cstddef>
 #include <string>
 #include <string_view>
 #include <vector>
-class CloudOSNativeAppsWindow final {
+
+class CloudOSNativeAppsWindow final
+{
 public:
- static void Open(HINSTANCE instance);
- static LRESULT CALLBACK WindowProcedure(HWND,UINT,WPARAM,LPARAM);
+    struct AppEntry final
+    {
+        std::wstring name;
+        std::wstring path;
+    };
+
+    static void Open(HINSTANCE instance);
+    static LRESULT CALLBACK WindowProcedure(
+        HWND window,
+        UINT message,
+        WPARAM w_param,
+        LPARAM l_param);
+
 private:
- struct AppEntry final { std::wstring name; std::wstring path; };
- explicit CloudOSNativeAppsWindow(HINSTANCE instance); ~CloudOSNativeAppsWindow()=default;
- bool Create(); void Layout(); void LoadCatalog(); void EnumerateFolder(const std::wstring&,int); void ApplyFilter(); void LaunchSelection();
- static std::wstring ReadText(HWND); static bool ContainsInsensitive(std::wstring_view,std::wstring_view);
- LRESULT HandleMessage(UINT,WPARAM,LPARAM);
- HINSTANCE instance_{}; HWND window_{}; HWND search_edit_{}; HWND list_{}; HWND launch_button_{};
- std::vector<AppEntry> catalog_; std::vector<std::size_t> visible_indices_;
+    explicit CloudOSNativeAppsWindow(HINSTANCE instance);
+    ~CloudOSNativeAppsWindow() = default;
+
+    CloudOSNativeAppsWindow(const CloudOSNativeAppsWindow&) = delete;
+    CloudOSNativeAppsWindow& operator=(const CloudOSNativeAppsWindow&) = delete;
+
+    bool Create();
+    void Layout();
+    void LoadCatalog();
+    void EnumerateFolder(const std::wstring& folder, int depth);
+    void ApplyFilter();
+    void LaunchSelection();
+
+    static std::wstring ReadText(HWND edit);
+    static bool ContainsInsensitive(
+        std::wstring_view text,
+        std::wstring_view query);
+
+    LRESULT HandleMessage(UINT message, WPARAM w_param, LPARAM l_param);
+
+    HINSTANCE instance_{};
+    HWND window_{};
+    HWND search_edit_{};
+    HWND list_{};
+    HWND launch_button_{};
+    std::vector<AppEntry> catalog_;
+    std::vector<std::size_t> visible_indices_;
 };
