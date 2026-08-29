d13a16842a7e10d00bdc9d861176940227c199bf
denevieirazz <douglasvieira2544@gmail.com>
2026-08-29 15:42:58 -0300

    chore(native): drop misleading empty shell module stubs

--- desktop/CloudOS.NativeShell/src/native_dash_window.cpp
+++
@@ -1,8 +0,0 @@
-#include "native_dash_window.h"
-namespace CloudOS {
-CloudOSNativeDashWindow::~CloudOSNativeDashWindow() {}
-bool CloudOSNativeDashWindow::Create(HINSTANCE, CloudOSNativeWindowManager*) { return true; }
-void CloudOSNativeDashWindow::Destroy() {}
-void CloudOSNativeDashWindow::UpdateLayout(const RECT&) {}
-void CloudOSNativeDashWindow::Redraw() {}
-}
