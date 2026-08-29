4d25c51dc32c34fa2cb093dec4c183f6f0e9f23a
denevieirazz <douglasvieira2544@gmail.com>
2026-08-29 15:41:57 -0300

    chore(native): remove empty taskbar placeholder

--- desktop/CloudOS.NativeShell/src/native_taskbar_window.cpp
+++
@@ -1,9 +0,0 @@
-#include "native_taskbar_window.h"
-namespace CloudOS {
-CloudOSNativeTaskbarWindow::~CloudOSNativeTaskbarWindow() {}
-bool CloudOSNativeTaskbarWindow::Create(HINSTANCE, CloudOSNativeWindowManager*) { return true; }
-void CloudOSNativeTaskbarWindow::Destroy() {}
-int CloudOSNativeTaskbarWindow::HeightPixels() const noexcept { return 0; }
-void CloudOSNativeTaskbarWindow::UpdateLayout(const RECT&) {}
-void CloudOSNativeTaskbarWindow::Redraw() {}
-}
