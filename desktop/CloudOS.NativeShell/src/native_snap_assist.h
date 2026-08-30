298d8849ca5d52b7b41b37a699982403c0e8f2ba
denevieirazz <douglasvieira2544@gmail.com>
2026-08-29 21:35:51 -0300

    feat(wm): add edge-driven Snap Assist controller

---
+++ desktop/CloudOS.NativeShell/src/native_snap_assist.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <Windows.h>
+
+class CloudOSNativeWindowManager;
+
+namespace CloudOS
+{
+class NativeSnapAssist final
+{
+public:
+    NativeSnapAssist() = default;
+    ~NativeSnapAssist();
+
+    NativeSnapAssist(const NativeSnapAssist&) = delete;
+    NativeSnapAssist& operator=(const NativeSnapAssist&) = delete;
+
+    bool Start(HINSTANCE instance, CloudOSNativeWindowManager* window_manager);
+    void Stop() noexcept;
+
+private:
+    enum class Zone
+    {
+        None,
+        Maximize,
+        LeftHalf,
+        RightHalf,
+        TopLeftQuarter,
+        TopRightQuarter,
+        BottomLeftQuarter,
+        BottomRightQuarter,
+        LeftThird,
+        CenterThird,
+        RightThird,
+        LeftTwoThirds,
+        RightTwoThirds,
+    };
+
+    static void CALLBACK WinEventCallback(
+        HWINEVENTHOOK hook,
+        DWORD event,
+        HWND window,
+        LONG object_id,
+        LONG child_id,
+        DWORD event_thread,
+        DWORD event_time);
+    static LRESULT CALLBACK OverlayProcedure(HWND window, UINT message, WPARAM w_param, LPARAM l_param);
+
+    bool EnsureOverlay();
+    void BeginMove(HWND window);
+    void UpdateMove(HWND window);
+    void EndMove(HWND window);
+    void HideOverlay() noexcept;
+    void ShowOverlay(const RECT& target);
+    Zone ResolveZone(HWND window, POINT cursor, RECT* target) const;
+    bool ApplyZone(HWND window, Zone zone, const RECT& target);
+    static bool IsCandidate(HWND window);
+
+    HINSTANCE instance_{};
+    CloudOSNativeWindowManager* window_manager_{};
+    HWINEVENTHOOK move_hook_{};
+    HWINEVENTHOOK location_hook_{};
+    HWND overlay_{};
+    HWND moving_window_{};
+    Zone active_zone_{Zone::None};
+    RECT active_target_{};
+
+    static NativeSnapAssist* active_instance_;
+};
+} // namespace CloudOS
