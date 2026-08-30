43cf1697a9576264bfa3ec7179d959788afe627d
denevieirazz <douglasvieira2544@gmail.com>
2026-08-30 17:22:53 -0300

    feat(shell-v8): add snap layout flyout state

--- desktop/CloudOS.NativeShell/src/native_snap_assist.h
+++ desktop/CloudOS.NativeShell/src/native_snap_assist.h
@@ -45,14 +45,18 @@ private:
         DWORD event_thread,
         DWORD event_time);
     static LRESULT CALLBACK OverlayProcedure(HWND window, UINT message, WPARAM w_param, LPARAM l_param);
+    static LRESULT CALLBACK LayoutProcedure(HWND window, UINT message, WPARAM w_param, LPARAM l_param);
 
     bool EnsureOverlay();
+    bool EnsureLayoutFlyout();
     void BeginMove(HWND window);
     void UpdateMove(HWND window);
     void EndMove(HWND window);
     void HideOverlay() noexcept;
     void ShowOverlay(const RECT& target);
-    Zone ResolveZone(HWND window, POINT cursor, RECT* target) const;
+    void HideLayoutFlyout() noexcept;
+    void ShowLayoutFlyout(const RECT& work, Zone selected);
+    Zone ResolveZone(HWND window, POINT cursor, RECT* target, bool* show_layout) const;
     bool ApplyZone(HWND window, Zone zone, const RECT& target);
     static bool IsCandidate(HWND window);
 
@@ -61,9 +65,12 @@ private:
     HWINEVENTHOOK move_hook_{};
     HWINEVENTHOOK location_hook_{};
     HWND overlay_{};
+    HWND layout_flyout_{};
     HWND moving_window_{};
     Zone active_zone_{Zone::None};
+    Zone layout_zone_{Zone::None};
     RECT active_target_{};
+    RECT layout_work_{};
 
     static NativeSnapAssist* active_instance_;
 };
