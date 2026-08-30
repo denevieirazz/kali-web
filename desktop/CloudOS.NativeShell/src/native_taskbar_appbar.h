86dd31d7ecf5f682a73ba116391c4bd957f7f387
denevieirazz <douglasvieira2544@gmail.com>
2026-08-29 23:23:55 -0300

    feat(taskbar): prepare grouping, persistent pins and contextual controls

--- desktop/CloudOS.NativeShell/src/native_taskbar_appbar.h
+++ desktop/CloudOS.NativeShell/src/native_taskbar_appbar.h
@@ -6,10 +6,20 @@
 #include <string>
 #include <vector>
 
+#include "native_shell_pins.h"
 #include "native_window_manager.h"
 
 namespace CloudOS
 {
+constexpr UINT CLOUDOS_WM_TASKBAR_QUERY_HIT = WM_APP + 0x492;
+
+struct CloudOSTaskbarHitQuery final
+{
+    POINT client_point{};
+    RECT task_rect{};
+    HWND window{};
+};
+
 class CloudOSTaskbarAppBar final
 {
 public:
@@ -36,9 +46,27 @@ public:
     void SetNotificationsCallback(AnchorCallback callback) { on_notifications_ = std::move(callback); }
 
 private:
+    struct TaskGroup final
+    {
+        DWORD process_id{};
+        std::vector<HWND> windows;
+        std::wstring title;
+    };
+
     void Paint();
     void RebuildHitTargets();
+    void ReloadPins();
     void LaunchPinned(std::size_t index);
+    void LaunchPin(const ShellPinItem& pin);
+    void ShowPinnedContextMenu(std::size_t index, POINT screen_point);
+    void ShowTaskContextMenu(std::size_t index, POINT screen_point);
+    void ShowTaskGroupPicker(std::size_t index, POINT screen_point);
+    void ActivateTaskGroup(std::size_t index);
+    void MoveTaskToWorkspace(HWND window, int workspace);
+    void CloseTaskGroup(const TaskGroup& group);
+    [[nodiscard]] int FindCloudApp(std::wstring_view id) const;
+    [[nodiscard]] std::wstring PinTitle(const ShellPinItem& pin) const;
+    [[nodiscard]] HWND HitTaskWindow(POINT point, RECT* bounds) const;
     LRESULT HandleMessage(HWND window, UINT message, WPARAM w_param, LPARAM l_param);
     static LRESULT CALLBACK WindowProcedure(HWND window, UINT message, WPARAM w_param, LPARAM l_param);
 
@@ -55,12 +83,14 @@ private:
     RECT clock_rect_{};
     std::vector<RECT> workspace_rects_;
     std::vector<RECT> pinned_rects_;
-    std::vector<int> pinned_app_indices_;
+    std::vector<ShellPinItem> pinned_items_;
     std::vector<RECT> task_rects_;
-    std::vector<HWND> task_windows_;
+    std::vector<TaskGroup> task_groups_;
 
     int hovered_kind_{-1};
     int hovered_index_{-1};
+    int drag_pin_index_{-1};
+    bool drag_pin_moved_{};
     bool tracking_mouse_{};
 
     AnchorCallback on_start_;
