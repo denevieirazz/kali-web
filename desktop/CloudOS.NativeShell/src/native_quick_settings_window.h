491a9c6df8f189c2761986c1e3247ca0ba9329ac
denevieirazz <douglasvieira2544@gmail.com>
2026-08-30 12:48:07 -0300

    feat(platform): compile Bluetooth V7 provider with Quick Settings

--- desktop/CloudOS.NativeShell/src/native_quick_settings_window.h
+++ desktop/CloudOS.NativeShell/src/native_quick_settings_window.h
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "native_audio_mixer_v7.h"
+#include "native_bluetooth_v7.h"
 #include "native_media_control_v7.h"
 #include "native_system_control_backend.h"
 #include "native_windows_search_v7.h"
