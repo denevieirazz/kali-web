3149617ef77bd8b774482fccb74d05d2a9fd1395
denevieirazz <douglasvieira2544@gmail.com>
2026-08-30 02:16:16 -0300

    feat(control-plane): add persisted appearance manager

---
+++ desktop/CloudOS.NativeShell/src/native_appearance_manager.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <windows.h>
+
+#include <array>
+
+namespace CloudOS
+{
+struct NativeAppearanceState final
+{
+    COLORREF accent{RGB(99, 102, 241)};
+    bool transparency{true};
+    bool compact_status{};
+};
+
+class NativeAppearanceManager final
+{
+public:
+    static NativeAppearanceState Current();
+    static void SetAccent(COLORREF accent);
+    static void SetTransparency(bool enabled);
+    static void SetCompactStatus(bool enabled);
+    static COLORREF Accent();
+    static COLORREF AccentHover();
+    static COLORREF AccentSubtle();
+    static COLORREF NextPresetAccent(COLORREF current);
+    static std::array<COLORREF, 6> Presets();
+
+private:
+    static NativeAppearanceState Load();
+    static void Save(const NativeAppearanceState& state);
+};
+} // namespace CloudOS
