0c11a2a4b006cfba47ae29d67360c3a494f888af
denevieirazz <douglasvieira2544@gmail.com>
2026-08-29 22:54:39 -0300

    refactor(desktop): remove dead monolithic Start and dock state from Shell V3 desktop

--- desktop/CloudOS.NativeShell/src/native_desktop_window.h
+++ desktop/CloudOS.NativeShell/src/native_desktop_window.h
@@ -2,9 +2,7 @@
 
 #include <windows.h>
 
-#include <array>
 #include <functional>
-#include <string>
 #include <vector>
 
 #include "native_system_stats.h"
@@ -13,6 +11,9 @@
 
 namespace CloudOS
 {
+// Shell V3 desktop is intentionally narrow: wallpaper, desktop namespace,
+// first-party shortcuts and input forwarding. Start, AppBar/taskbar and flyouts
+// are separate HWND components and must not creep back into this class.
 class CloudOSNativeDesktopWindow final
 {
 public:
@@ -38,22 +39,10 @@ public:
 
 private:
     void Paint();
-    void OnSearchChanged();
-    void SelectFocusedApp();
     void ActivateAppIndex(int app_index);
     void RefreshWorkArea();
-    void SetStartMenuOpen(bool open, bool focus_search = true);
-    void ToggleStartMenu();
-    bool IsPointClickable(POINT point) const;
 
     LRESULT HandleMessage(HWND window, UINT message, WPARAM w_param, LPARAM l_param);
-    static LRESULT CALLBACK SearchSubclass(
-        HWND window,
-        UINT message,
-        WPARAM w_param,
-        LPARAM l_param,
-        UINT_PTR subclass_id,
-        DWORD_PTR reference_data);
     static LRESULT CALLBACK WindowProcedure(
         HWND window,
         UINT message,
@@ -61,36 +50,11 @@ private:
         LPARAM l_param);
 
     HWND hwnd_{};
-    HWND search_edit_{};
-    HFONT search_font_{};
-    HBRUSH edit_bg_brush_{};
     HINSTANCE instance_{};
     CloudOSNativeWindowManager* window_manager_{nullptr};
 
-    std::wstring search_query_;
-    std::vector<int> filtered_indices_;
-    std::vector<RECT> app_grid_rects_;
     std::vector<RECT> quick_launch_rects_;
     std::vector<int> quick_launch_app_indices_;
-    std::vector<RECT> dock_rects_;
-    std::vector<int> dock_app_indices_;
-    std::vector<TaskHit> task_hits_;
-    std::array<RECT, 4> workspace_rects_{};
-
-    int focused_app_index_{0};
-    int hovered_app_index_{-1};
-    int hovered_widget_id_{-1};
-    int hovered_dock_id_{-1};
-    bool tracking_mouse_{false};
-    bool start_menu_open_{false};
-
-    RECT start_button_rect_{};
-    RECT start_menu_rect_{};
-    RECT power_button_rect_{};
-    RECT desktop_status_rect_{};
-    RECT system_button_rect_{};
-    RECT clock_rect_{};
-
     SystemStats current_stats_{};
 
     ActionCallback on_action_;
