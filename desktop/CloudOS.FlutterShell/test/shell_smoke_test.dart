ce25439261594a3eaeb917509b5d5420ef82c348
denevieirazz <douglasvieira2544@gmail.com>
2026-08-31 17:46:37 -0300

    test(flutter-v19): disambiguate close button finder in smoke test

--- desktop/CloudOS.FlutterShell/test/shell_smoke_test.dart
+++ desktop/CloudOS.FlutterShell/test/shell_smoke_test.dart
@@ -34,7 +34,7 @@ void main() {
       expect(find.text('Visual Studio Code'), findsOneWidget);
 
       // Close Start
-      await tester.tap(find.byTooltip('Fechar (Esc)'));
+      await tester.tap(find.byTooltip('Fechar (Esc)').first);
       await tester.pumpAndSettle();
       expect(find.text('CloudOS Start'), findsNothing);
 
