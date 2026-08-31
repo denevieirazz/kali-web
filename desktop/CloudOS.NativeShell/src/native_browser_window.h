4debeda7c8a8383688b453f2843314aefcf2c4f8
denevieirazz <douglasvieira2544@gmail.com>
2026-08-31 10:49:01 -0300

    fix(browser-v16): track WebView2 download capability through ICoreWebView2_4

--- desktop/CloudOS.NativeShell/src/native_browser_window.h
+++ desktop/CloudOS.NativeShell/src/native_browser_window.h
@@ -52,6 +52,10 @@ private:
     Microsoft::WRL::ComPtr<ICoreWebView2Environment> environment_;
     Microsoft::WRL::ComPtr<ICoreWebView2Controller> controller_;
     Microsoft::WRL::ComPtr<ICoreWebView2> webview_;
+    // DownloadStarting was introduced on ICoreWebView2_4. Keep the base
+    // interface for common navigation and hold the versioned capability
+    // separately so older/runtime capability failures degrade cleanly.
+    Microsoft::WRL::ComPtr<ICoreWebView2_4> webview_v4_;
     EventRegistrationToken navigation_completed_token_{};
     EventRegistrationToken history_changed_token_{};
     EventRegistrationToken download_starting_token_{};
