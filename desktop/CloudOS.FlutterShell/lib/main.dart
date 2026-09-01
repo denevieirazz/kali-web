7a47651e8b7fdf0f8f8e6f73cd1ea1fa04a4f0a6
denevieirazz <douglasvieira2544@gmail.com>
2026-09-01 05:54:03 -0300

    fix(flutter): preserve CloudOSApp public import contract

--- desktop/CloudOS.FlutterShell/lib/main.dart
+++ desktop/CloudOS.FlutterShell/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/widgets.dart';
 
 import 'app/cloudos_app.dart';
 
+export 'app/cloudos_app.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const CloudOSApp());
