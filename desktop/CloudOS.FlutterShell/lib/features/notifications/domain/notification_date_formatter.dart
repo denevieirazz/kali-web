350652129ca10145cf18083c7f44a6be4b5ea530
denevieirazz <douglasvieira2544@gmail.com>
2026-09-01 06:38:02 -0300

    refactor(notifications): extract date formatter

---
+++ desktop/CloudOS.FlutterShell/lib/features/notifications/domain/notification_date_formatter.dart
@@ -0,0 +1,27 @@
+String formatNotificationDate(DateTime date) {
+  const weekdays = <String>[
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado',
+    'Domingo',
+  ];
+  const months = <String>[
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ];
+
+  return '${weekdays[date.weekday - 1]}, ${date.day} de ${months[date.month - 1]}';
+}
