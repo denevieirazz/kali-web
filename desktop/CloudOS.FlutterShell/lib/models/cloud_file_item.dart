02358ab4ca626e41f7f3bca466136a824b59fb13
denevieirazz <douglasvieira2544@gmail.com>
2026-09-01 06:39:08 -0300

    refactor(models): extract CloudFileItem model

---
+++ desktop/CloudOS.FlutterShell/lib/models/cloud_file_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+enum CloudFileSource { cloudDrive, windows, linux, trash }
+
+class CloudFileItem {
+  const CloudFileItem({
+    required this.name,
+    required this.path,
+    required this.isFolder,
+    required this.sizeFormatted,
+    required this.modifiedFormatted,
+    required this.source,
+    this.icon,
+    this.extension,
+  });
+
+  final String name;
+  final String path;
+  final bool isFolder;
+  final String sizeFormatted;
+  final String modifiedFormatted;
+  final CloudFileSource source;
+  final IconData? icon;
+  final String? extension;
+}
