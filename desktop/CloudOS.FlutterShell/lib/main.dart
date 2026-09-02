00cfad34798fa450685214d7d148931851e3f0aa
denevieirazz <douglasvieira2544@gmail.com>
2026-09-02 13:07:52 -0300

    feat(lifecycle): guard native close with durable Session V3 flush

--- desktop/CloudOS.FlutterShell/lib/main.dart
+++ desktop/CloudOS.FlutterShell/lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'core/cloudos_theme.dart';
+import 'services/app_lifecycle_coordinator.dart';
 import 'services/runtime_event_service.dart';
 import 'shell/cloudos_shell.dart';
 
@@ -10,8 +13,32 @@ void main() {
   runApp(const CloudOSApp());
 }
 
-class CloudOSApp extends StatelessWidget {
+class CloudOSApp extends StatefulWidget {
   const CloudOSApp({super.key});
+
+  @override
+  State<CloudOSApp> createState() => _CloudOSAppState();
+}
+
+class _CloudOSAppState extends State<CloudOSApp> {
+  late final AppLifecycleListener _lifecycleListener;
+
+  @override
+  void initState() {
+    super.initState();
+    _lifecycleListener = AppLifecycleListener(
+      onExitRequested: AppLifecycleCoordinator.instance.handleExitRequest,
+      onPause: () => unawaited(AppLifecycleCoordinator.instance.checkpoint()),
+      onHide: () => unawaited(AppLifecycleCoordinator.instance.checkpoint()),
+      onDetach: () => unawaited(AppLifecycleCoordinator.instance.checkpoint()),
+    );
+  }
+
+  @override
+  void dispose() {
+    _lifecycleListener.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
